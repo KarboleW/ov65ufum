9958f76a26f25724be6172d11492a2d9d2f68694
Wenke Karbole <wenke.karbole@fau.de>
2021-11-10 20:11:41 +0100

    comments: commit next task

---
+++ ov65ufum/function.py
@@ -0,0 +1,16 @@
+import numpy as np
+from ipywidgets import interact, fixed
+from PIL import Image
+
+def imshow(X, resize=None):
+    """
+    You should create a way to resize an image from an array X.
+    The use of widgets is optional but you can take a look to interact.
+    We should be able to install this package in Google Colab from your Git
+    repo.
+    """
+    
+    image = Image.fromarray(X)
+    image = image.resize(resize)
+    
+    return image
