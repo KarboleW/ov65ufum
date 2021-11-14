a9a231f1647f10d07740aa264b6505e51cf7a601
KarboleW <94073714+KarboleW@users.noreply.github.com>
2021-11-14 17:51:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 from setuptools import find_packages
-setup(name='idm_id',
+setup(name='ov65ufum',
  version='0.1',
- author='DSSS',
- author_email='name@fau.de',
+ author='KarboleW',
+ author_email='karbole.wenke@fau.de',
  packages=find_packages(),
  install_requires=['numpy', 'Pillow', 'ipywidgets'])
